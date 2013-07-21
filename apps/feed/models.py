5f7e6282db826832f503167dcdad4b0206ead7fc
Henry Xie <henry@yipit.com>
2013-07-21 14:03:06 -0400

    creating first Post model

---
+++ apps/feed/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+def image_file_name(instance, filename):
+    filename, ext = filename.split('.')
+    code = instance.slug or User.objects.make_random_password()
+    filename = "{}.{}".format(code, ext)
+    return 'img/post/{}'.format(filename)
+
+
+class Post(models.Model):
+    user = models.ForeignKey('auth.User')
+    note = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    image = models.ImageField(upload_to=image_file_name, blank=True)
+    video_url = models.URLField(max_length=400, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
