7c9f58dd0ff87e292b6cd7d753296cacb6d34eb5
Henry Xie <henry@yipit.com>
2013-07-21 16:13:13 -0400

    applying textarea widget to input box

--- apps/feed/forms.py
+++ apps/feed/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from .models import Post
 
 
@@ -6,3 +6,6 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ('user', 'note', 'image', 'video_url')
+        widgets = {
+            'note': Textarea(attrs={'cols': 80, 'rows': 3}),
+        }
