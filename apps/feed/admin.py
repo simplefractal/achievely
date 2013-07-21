687bc56889cb54902aabf60135bcdafb7f8b7f72
Henry Xie <henry@yipit.com>
2013-07-21 15:02:18 -0400

    better post admin

--- apps/feed/admin.py
+++ apps/feed/admin.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.contrib import admin
-
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
+    fields = ('user', 'note', 'image', 'video_url')
+    list_display = ('user', 'note', 'image', 'video_url', 'date_added')
+    search_fields = ['note']
+
+
+admin.site.register(Post, PostAdmin)
