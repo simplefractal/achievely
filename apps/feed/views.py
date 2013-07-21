700281a6cfc29117df5a60e485ca82297bd43417
Henry Xie <henry@yipit.com>
2013-07-21 14:20:12 -0400

    adding AddPostView

--- apps/feed/views.py
+++ apps/feed/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.views.generic import View
+
+from .forms import PostForm
 
 
 def home(request):
@@ -7,3 +10,15 @@ def home(request):
     context = {}
     return render_to_response(template,
                               RequestContext(request, context))
+
+
+class AddPostView(View):
+
+    def post(self, request, *args, **kwargs):
+        form_data = request.POST.copy()
+
+        form = PostForm(form_data, request.FILES)
+        if form.is_valid():
+            form.save()
+
+        return HttpResponseRedirect(reverse('home'))
--- urls.py
+++ urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-
 from django.contrib import admin
 admin.autodiscover()
 
+from feed.views import AddPostView
+
+
 urlpatterns = patterns('',
     url(r'^$', 'feed.views.home', name='home'),
+    url(r'^add_post/$', AddPostView.as_view(), name='add_post'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
