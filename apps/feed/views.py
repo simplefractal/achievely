9d85daa1dfe2151710caa0981bacf1d7c1a0dbd3
suneel0101 <suneel0101@gmail.com>
2013-07-23 21:15:33 -0400

    removing form from feed

--- apps/feed/views.py
+++ apps/feed/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.views.generic import TemplateView
 
-from .forms import PostForm
 from .models import Post
 
 
@@ -11,22 +10,8 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         return {
-            'form': PostForm(),
             'posts': Post.objects.all().order_by('-id')
         }
-
-    def post(self, request, *args, **kwargs):
-        form_data = {}
-        form_data['user'] = request.user.id
-        form_data['note'] = request.POST.get('note')
-        form_data['video_url'] = request.POST.get('video_url')
-
-        form = PostForm(form_data, request.FILES)
-
-        if form.is_valid():
-            form.save()
-
-        return self.get(request, *args, **kwargs)
 
 
 class SingleFeedPostView(TemplateView):
